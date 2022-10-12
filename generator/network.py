e8de1be8aa784eda03fbfc3f6713496583704b22
sataketatsuya <satake11214@gmail.com>
2022-10-12 09:45:23 +0900

    change generator networks

--- generator/network.py
+++ generator/network.py
@@ -12,7 +12,7 @@ class FeedForwardNN(nn.Module):
     """
         A standard in_dim-64-64-out_dim Feed Forward Neural Network.
     """
-    def __init__(self, in_dim, out_dim, dropout=0.1):
+    def __init__(self, in_dim, out_dim, device, dropout=0.1):
         """
             Initialize the network and set up the layers.
 
@@ -24,6 +24,8 @@ class FeedForwardNN(nn.Module):
                 None
         """
         super(FeedForwardNN, self).__init__()
+        
+        self.device = device
 
         self.layer1 = nn.Linear(in_dim, 1024)
         self.dropout1 = nn.Dropout(dropout)
@@ -33,21 +35,17 @@ class FeedForwardNN(nn.Module):
         self.dropout3 = nn.Dropout(dropout)
         self.layer4 = nn.Linear(32, out_dim)
 
-    def forward(self, obs):
+    def forward(self, state):
         """
             Runs a forward pass on the neural network.
 
             Parameters:
-                obs - observation to pass as input
+                state - state to pass as input
 
             Return:
                 output - the output of our forward pass
         """
-        # Convert observation to tensor if it's a numpy array
-        if isinstance(obs, np.ndarray):
-            obs = torch.tensor(obs, dtype=torch.float)
-
-        activation1 = F.relu(self.dropout1(self.layer1(obs)))
+        activation1 = F.relu(self.dropout1(self.layer1(state)))
         activation2 = F.relu(self.dropout2(self.layer2(activation1)))
         activation3 = F.relu(self.dropout3(self.layer3(activation2)))
         output = self.layer4(activation3)
